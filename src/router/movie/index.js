d48b0eef3cbf82f0a2940643a86b518c1f7445d3
ttai <721482964@qq.com>
2021-01-31 17:17:43 +0800

    组件创建完成

---
+++ src/router/movie/index.js
@@ -0,0 +1,11 @@
+export default {
+  path: '/movie',
+  component: () => import('@/views/Movie'),
+  children: [
+    { path: 'city', component: () => import('@/components/City') },
+    { path: 'now', component: () => import('@/components/Now') },
+    { path: 'coming', component: () => import('@/components/Coming') },
+    { path: 'search', component: () => import('@/components/Search') },
+    { path: '/movie', redirect: '/movie/now' }
+  ]
+}
